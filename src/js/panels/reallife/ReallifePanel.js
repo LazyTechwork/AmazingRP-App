027b53f005bd6378cadd87811927cdb5491bc2d6
Alexeev-tech <alexeev.tech@gmail.com>
2020-07-12 16:27:19 +0300

    Update images

--- src/js/panels/reallife/ReallifePanel.js
+++ src/js/panels/reallife/ReallifePanel.js
@@ -14,9 +14,29 @@ const beforeafter = [
         crmp: "https://sun9-38.userapi.com/E8GS2_hnvIrqaKLF1oPCDp_IZ2RHh3PGIZkeiw/Fgyw5mFLN70.jpg",
         real: "https://sun9-22.userapi.com/gRd5c2HHG5wGw5jm8JJjejpKbKfRqtN8uyFS3w/-sXkH97Dewo.jpg"
     }, {
-        name: 'Магазин «Перекрёсток»',
-        crmp: "https://sun9-38.userapi.com/E8GS2_hnvIrqaKLF1oPCDp_IZ2RHh3PGIZkeiw/Fgyw5mFLN70.jpg",
-        real: "https://sun9-22.userapi.com/gRd5c2HHG5wGw5jm8JJjejpKbKfRqtN8uyFS3w/-sXkH97Dewo.jpg"
+        name: 'Стадион в г. Арзамасе',
+        crmp: "https://sun6-13.userapi.com/HKva-XWPT6JcSVzJ8osauO3NVmh5Eq57aVVpLQ/MNFWIhyuE2U.jpg",
+        real: "https://sun6-14.userapi.com/_c9rIq331MiHPl2EYSER-AE6WliVoBrU72sLog/DEq0zMFJ49E.jpg"
+    }, {
+        name: 'Бусаево',
+        crmp: "https://sun6-19.userapi.com/9eZ_0QaNI-YyGCpRUSAQHLjnPB-nuIJV0z8GZQ/hEH-a-U4cVc.jpg",
+        real: "https://sun6-19.userapi.com/577BVdth1wumTZe2u9QRR6mRud1LJVuephcdQA/PNKKX3yVVPo.jpg"
+    }, {
+        name: 'Башня «Останкино»',
+        crmp: "https://sun6-16.userapi.com/tBbE70FZMd1BR5Az0CL38KSFBRDTGjlMn8DwCw/KQ6Miqc96tU.jpg",
+        real: "https://sun6-13.userapi.com/Qvj61Do83Un8lExjP4fKBP0GZpMsrBvdAIRaqg/dCGJ1SsvXiw.jpg"
+    }, {
+        name: '«Администрация» в пгт. Батырево',
+        crmp: "https://sun6-16.userapi.com/RyWE6NZjWcpuhlxEJVdFOJgjbkL7zAqIM6UGVQ/1wZtxSGlVq4.jpg",
+        real: "https://sun6-16.userapi.com/U-pizLAesZRAUSNhV0suVERfrxuaKcKgs2Xziw/pZFrGZ5Wk-E.jpg"
+    }, {
+        name: 'Банк «Сбербанк»',
+        crmp: "https://sun6-13.userapi.com/1HjG7Wu31uRJYVNI0zD7z7UHLPTgM5gKu4i9mQ/Yn3FBIGSNfU.jpg",
+        real: "https://sun6-13.userapi.com/UJxjievLYKPEe43Bk7RtWEcrmJcx02r9J_ROGA/i9N-HrI23A0.jpg"
+    }, {
+        name: 'Маяк',
+        crmp: "https://sun6-13.userapi.com/AC0izLZF_gRMx514aYEqfqPGV_MsaetJXc8yMw/9lqXp3sLRKo.jpg",
+        real: "https://sun6-16.userapi.com/ugci9pTAdMsg3MFCRNUpU7hy6-NQqBFvlJqHBQ/K5xMZWb-qn0.jpg"
     },
 ]
 
