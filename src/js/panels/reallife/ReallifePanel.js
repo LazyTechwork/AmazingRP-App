e28f30b477e6d3ff8e2e845ec7a92cad21e76298
Alexeev-tech <alexeev.tech@gmail.com>
2020-07-12 18:31:23 +0300

    New hosting images

--- src/js/panels/reallife/ReallifePanel.js
+++ src/js/panels/reallife/ReallifePanel.js
@@ -11,32 +11,32 @@ import {setFormData} from "../../store/formData/actions";
 const beforeafter = [
     {
         name: 'Магазин «Перекрёсток»',
-        crmp: "https://sun9-38.userapi.com/E8GS2_hnvIrqaKLF1oPCDp_IZ2RHh3PGIZkeiw/Fgyw5mFLN70.jpg",
-        real: "https://sun9-22.userapi.com/gRd5c2HHG5wGw5jm8JJjejpKbKfRqtN8uyFS3w/-sXkH97Dewo.jpg"
+        crmp: "https://i.ibb.co/GWCmc4N/Fgyw5m-FLN70.jpg",
+        real: "https://i.ibb.co/f2rWrGg/s-Xk-H97-Dewo.jpg"
     }, {
         name: 'Стадион в г. Арзамасе',
-        crmp: "https://sun6-13.userapi.com/HKva-XWPT6JcSVzJ8osauO3NVmh5Eq57aVVpLQ/MNFWIhyuE2U.jpg",
-        real: "https://sun6-14.userapi.com/_c9rIq331MiHPl2EYSER-AE6WliVoBrU72sLog/DEq0zMFJ49E.jpg"
+        crmp: "https://i.ibb.co/DWHXZB4/MNFWIhyu-E2-U.jpg",
+        real: "https://i.ibb.co/h8ZGmV3/DEq0z-MFJ49-E.jpg"
     }, {
         name: 'Бусаево',
-        crmp: "https://sun6-19.userapi.com/9eZ_0QaNI-YyGCpRUSAQHLjnPB-nuIJV0z8GZQ/hEH-a-U4cVc.jpg",
-        real: "https://sun6-19.userapi.com/577BVdth1wumTZe2u9QRR6mRud1LJVuephcdQA/PNKKX3yVVPo.jpg"
+        crmp: "https://i.ibb.co/T1hG2yW/h-EH-a-U4c-Vc.jpg",
+        real: "https://i.ibb.co/Z6hXGpz/PNKKX3y-VVPo.jpg"
     }, {
         name: 'Башня «Останкино»',
-        crmp: "https://sun6-16.userapi.com/tBbE70FZMd1BR5Az0CL38KSFBRDTGjlMn8DwCw/KQ6Miqc96tU.jpg",
-        real: "https://sun6-13.userapi.com/Qvj61Do83Un8lExjP4fKBP0GZpMsrBvdAIRaqg/dCGJ1SsvXiw.jpg"
+        crmp: "https://i.ibb.co/GPCDQYY/KQ6-Miqc96t-U.jpg",
+        real: "https://i.ibb.co/g7Bs6MW/d-CGJ1-Ssv-Xiw.jpg"
     }, {
         name: '«Администрация» в пгт. Батырево',
-        crmp: "https://sun6-16.userapi.com/RyWE6NZjWcpuhlxEJVdFOJgjbkL7zAqIM6UGVQ/1wZtxSGlVq4.jpg",
-        real: "https://sun6-16.userapi.com/U-pizLAesZRAUSNhV0suVERfrxuaKcKgs2Xziw/pZFrGZ5Wk-E.jpg"
+        crmp: "https://i.ibb.co/BymXDmM/1w-Ztx-SGl-Vq4.jpg",
+        real: "https://i.ibb.co/9Hm9Xg6/p-ZFr-GZ5-Wk-E.jpg"
     }, {
         name: 'Банк «Сбербанк»',
-        crmp: "https://sun6-13.userapi.com/1HjG7Wu31uRJYVNI0zD7z7UHLPTgM5gKu4i9mQ/Yn3FBIGSNfU.jpg",
-        real: "https://sun6-13.userapi.com/UJxjievLYKPEe43Bk7RtWEcrmJcx02r9J_ROGA/i9N-HrI23A0.jpg"
+        crmp: "https://i.ibb.co/QNks31p/Yn3-FBIGSNf-U.jpg",
+        real: "https://i.ibb.co/vPzZHfD/i9-N-Hr-I23-A0.jpg"
     }, {
         name: 'Маяк',
-        crmp: "https://sun6-13.userapi.com/AC0izLZF_gRMx514aYEqfqPGV_MsaetJXc8yMw/9lqXp3sLRKo.jpg",
-        real: "https://sun6-16.userapi.com/ugci9pTAdMsg3MFCRNUpU7hy6-NQqBFvlJqHBQ/K5xMZWb-qn0.jpg"
+        crmp: "https://i.ibb.co/rZTVvNJ/9lq-Xp3s-LRKo.jpg",
+        real: "https://i.ibb.co/8KdbPQB/K5x-MZWb-qn0.jpg"
     },
 ]
 
